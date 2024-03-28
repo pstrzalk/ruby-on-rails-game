9e96ca111653ead1b39d657e44fdda3ee0144bb5
Paweł Strzałkowski <p.strzalkowski@visuality.pl>
2024-03-29 00:26:42 +0100

    Model position as a value object

--- app/models/game/player.rb
+++ app/models/game/player.rb
@@ -4,9 +4,9 @@ class Game < ApplicationRecord
   class Player < ApplicationRecord
     broadcasts_to ->(player) { "game_player_#{player.id}" } unless Rails.env.test?
 
-    # composed_of :position,
-    #             class_name: 'Position',
-    #             mapping: [%w[position_horizontal horizontal], %w[position_vertical vertical]]
+    composed_of :position,
+                class_name: 'Position',
+                mapping: [%w[position_horizontal horizontal], %w[position_vertical vertical]]
 
     belongs_to :game, touch: true
 
@@ -15,23 +15,23 @@ class Game < ApplicationRecord
     end
 
     def move_left
-      self.position_horizontal -= 1
-      # self.position = Position.new(position.horizontal - 1, position.vertical)
+      # self.position_horizontal -= 1
+      self.position = Position.new(position.horizontal - 1, position.vertical)
     end
 
     def move_right
-      self.position_horizontal += 1
-      # self.position = Position.new(position.horizontal + 1, position.vertical)
+      # self.position_horizontal += 1
+      self.position = Position.new(position.horizontal + 1, position.vertical)
     end
 
     def move_forward
-      self.position_vertical += 1
-      # self.position = Position.new(position.horizontal, position.vertical + 1)
+      # self.position_vertical += 1
+      self.position = Position.new(position.horizontal, position.vertical + 1)
     end
 
     def move_back
-      self.position_vertical -= 1
-      # self.position = Position.new(position.horizontal, position.vertical - 1)
+      # self.position_vertical -= 1
+      self.position = Position.new(position.horizontal, position.vertical - 1)
     end
   end
 end
